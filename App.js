61dc42da7abdb4bb7efd1a658674c8c1eab357b6
mohitk58 <mk581991@gmail.com>
2025-09-27 06:30:30 +0530

    Episode 02 - Igniting our App

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child", key: "child1" }, [
     React.createElement(
       "h1",
-      { id: "heading", xyz: "abc" },
-      "Hello World from React!"
+      { id: "heading", xyz: "abc", key: 4 },
+      "Hello World from React! - 🚀"
     ),
     React.createElement(
       "h2",
-      { id: "heading", xyz: "abc" },
+      { id: "heading", xyz: "abc", key: 5 },
       "Hello World from React!"
     ),
   ]),
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child", key: "child2" }, [
     React.createElement(
       "h1",
-      { id: "heading", xyz: "abc" },
+      { id: "heading", xyz: "abc", key: 2 },
       "Hello World from React!"
     ),
     React.createElement(
       "h2",
-      { id: "heading", xyz: "abc" },
+      { id: "heading", xyz: "abc", key: 3 },
       "Hello World from React!"
     ),
   ]),
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+# Parcel
+
+- Dev build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused codes
+- Different dev and prod bundles

4c8b5d383b062e69e9942d40935ad83da489d4d9
mohitk58 <mk581991@gmail.com>
2025-10-29 07:16:08 +0530

    Episode 6.2 - CORS plugin  Issue Resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ const CDN_URL =
 const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All";
 const API_ENDPOINT =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+  "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
 
 export { CDN_URL, LOGO_URL, API_ENDPOINT }; // Named Export
